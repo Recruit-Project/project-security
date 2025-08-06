08a3601e4dd1293312261b47c891dca2cd9ac617
kimyj <yongjjun8718@gmail.com>
2025-08-06 21:24:29 +0900

    의존성 제거

--- src/main/java/com/kyj/fmk/sec/service/CustomOauth2UserService.java
+++ src/main/java/com/kyj/fmk/sec/service/CustomOauth2UserService.java
@@ -1,8 +1,6 @@
 package com.kyj.fmk.sec.service;
 
 import com.kyj.fmk.sec.dto.*;
-import com.kyj.fmk.sec.entity.UserEntity;
-import com.kyj.fmk.sec.repository.jpa.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -14,7 +12,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomOauth2UserService extends DefaultOAuth2UserService {
 
-    private final UserRepository userRepository;
+//    private final UserRepository userRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -33,39 +31,40 @@ public class CustomOauth2UserService extends DefaultOAuth2UserService {
             return null;
         }
 
-        String username = oAuth2Response.getProvider()+"_"+oAuth2Response.getProviderId();
-        UserEntity existData = userRepository.findByUsername(username);
-
-        if (existData == null) {
-
-            UserEntity userEntity = new UserEntity();
-            userEntity.setUsername(username);
-            userEntity.setEmail(oAuth2Response.getEmail());
-            userEntity.setName(oAuth2Response.getName());
-            userEntity.setRole("ROLE_USER");
-
-            userRepository.save(userEntity);
-
-            UserDTO userDTO = new UserDTO();
-            userDTO.setUsername(username);
-            userDTO.setName(oAuth2Response.getName());
-            userDTO.setRole("ROLE_USER");
-
-            return new CustomOAuth2User(userDTO);
-        }
-        else {
-
-            existData.setEmail(oAuth2Response.getEmail());
-            existData.setName(oAuth2Response.getName());
-
-            userRepository.save(existData);
-
-            UserDTO userDTO = new UserDTO();
-            userDTO.setUsername(existData.getUsername());
-            userDTO.setName(oAuth2Response.getName());
-            userDTO.setRole(existData.getRole());
-
-            return new CustomOAuth2User(userDTO);
-        }
+//        String username = oAuth2Response.getProvider()+"_"+oAuth2Response.getProviderId();
+//        UserEntity existData = userRepository.findByUsername(username);
+//
+//        if (existData == null) {
+//
+//            UserEntity userEntity = new UserEntity();
+//            userEntity.setUsername(username);
+//            userEntity.setEmail(oAuth2Response.getEmail());
+//            userEntity.setName(oAuth2Response.getName());
+//            userEntity.setRole("ROLE_USER");
+//
+//            userRepository.save(userEntity);
+//
+//            UserDTO userDTO = new UserDTO();
+//            userDTO.setUsername(username);
+//            userDTO.setName(oAuth2Response.getName());
+//            userDTO.setRole("ROLE_USER");
+//
+//            return new CustomOAuth2User(userDTO);
+//        }
+//        else {
+//
+//            existData.setEmail(oAuth2Response.getEmail());
+//            existData.setName(oAuth2Response.getName());
+//
+//            userRepository.save(existData);
+//
+//            UserDTO userDTO = new UserDTO();
+//            userDTO.setUsername(existData.getUsername());
+//            userDTO.setName(oAuth2Response.getName());
+//            userDTO.setRole(existData.getRole());
+//
+//            return new CustomOAuth2User(userDTO);
+//        }
+        return  null;
     }
 }
